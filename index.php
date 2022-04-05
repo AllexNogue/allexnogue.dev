efd22752a09a3e5faaae220ddd79b9f984d1ce1e
Allex Nogue <allexnogueira13@gmail.com>
2022-04-04 21:08:35 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Allex Nogue - Engenheiro de Software</title>
+    <meta name="description" content="Desenvolvedor de softwares">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg ">
+        <a class="navbar-brand" href="#">Allex Nogue</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <main role="main">
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3">
+            <div class="col-md-8 p-lg-5 mx-auto my-5">
+                <p class="mb-0 hi">Oi meu nome é</p>
+                <h1 class="my-name display-4 ">Allex Nogue</h1>
+                <h2 class="i-make lead ">Eu construo coisas para a web.</h2>
+                <p>Sou um engenheiro de software especializado em construir (e ocasionalmente projetar) experiências digitais excepcionais. Atualmente, estou focado em construir produtos acessíveis e centrados em soluções de pagamentos na Brasil Cash.</p>
+            </div>
+        </div>
+
+        <section id="about" class="position-relative overflow-hidden p-3 p-md-5 m-md-3 ">
+            <div class="col-md-8 p-lg-5 mx-auto my-5">
+                <h2 class="section-about">Sobre</h2>
+                <p>Olá! Meu nome é Brittany e eu gosto de criar coisas que vivem na internet. Meu interesse em desenvolvimento web começou em 2012, quando decidi tentar editar temas personalizados do Tumblr - acabou que criar um botão de reblog personalizado me ensinou muito sobre HTML e CSS!</p>
+            </div>
+        </section>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</body>
+
+</html>
